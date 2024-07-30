f0d6b615e8c37abc1b4449713bd23ce8061c5e50
Vee Jay <veejay.programming@gmail.com>
2024-07-30 16:20:25 +0800

    Added dynamic auth page title

--- auth/templates/head.php
+++ auth/templates/head.php
@@ -1,10 +1,22 @@
+<?php
+// Sets title based on file name
+function getPageTitle()
+{
+    $uri = $_SERVER["REQUEST_URI"];
+    $uri = ltrim($uri, '/auth');
+    $uri = str_replace('.php', '', $uri);
+    $uri = ucfirst($uri);
+    return $uri ?: 'Home';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= getPageTitle(); ?></title>
     <!-- CSS -->
     <link rel="stylesheet" href="css/style.css">
-    <title>Register</title>
 </head>
